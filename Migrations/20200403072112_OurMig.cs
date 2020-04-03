338d9793b550fbb976be7cad96104c07dfef213f
ketansharma97 <97.ketansharma@gmail.com>
2020-04-03 12:58:52 +0530

    stage 2

---
+++ Migrations/20200403072112_OurMig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DbGitPerson.Migrations
+{
+    public partial class OurMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Pid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Pname = table.Column<string>(nullable: true),
+                    Lname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Pid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
---
+++ Models/PersonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbGitPerson.Models
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
